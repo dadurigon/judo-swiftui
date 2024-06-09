50353a1d568fe166eb25de1d1ec3ea7ea1b0d136
Dion Durigon <dadurigon@icloud.com>
2024-06-09 18:15:02 -0400

    Fix delegate method

--- Sources/JudoDocument/Utils/CustomURLSession.swift
+++ Sources/JudoDocument/Utils/CustomURLSession.swift
@@ -19,6 +19,15 @@ public class CustomURLSession: URLSession {
 public class SessionDelegate: NSObject, URLSessionDelegate {
     
     // Bypass any challenge
+    public func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        
+        guard let serverTrust = challenge.protectionSpace.serverTrust else {
+            return completionHandler(URLSession.AuthChallengeDisposition.useCredential, nil)
+        }
+        return completionHandler(URLSession.AuthChallengeDisposition.useCredential, URLCredential(trust: serverTrust))
+    }
+    
+    // Bypass any challenge
     public func urlSession(_: URLSession, task _: URLSessionTask, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
         guard let serverTrust = challenge.protectionSpace.serverTrust else {
             return completionHandler(URLSession.AuthChallengeDisposition.useCredential, nil)
